d43d5be4600c068e890d5bb58a2179e4196815b4
natraj95 <natrajnagaraj95@gmail.com>
2023-09-14 14:48:32 +0530

    Sele13

--- src/org/sam/Fide.java
+++ src/org/sam/Fide.java
@@ -1,5 +1,12 @@
 package org.sam;
 
 public class Fide {
+	public void chess(){
+		System.out.println("queen");
+	}
+	public static void main(String[] args) {
+		Fide f = new Fide ();
+		f.chess();
+	}
 
 }
